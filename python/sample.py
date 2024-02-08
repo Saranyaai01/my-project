cd37e96e849e36a20bcf15131917e33b652a8328
Saranya N <saranyaai01@brightskills.com>
2024-02-08 13:37:04 +0530

    i work sample pgm try

---
+++ python/sample.py
@@ -0,0 +1,18 @@
+"""import calendar
+year = 2025
+print(calendar . calendar(int(year)))
+from turtle import*
+bgcolor("blue")
+color("red")
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()"""
+my_list=["",""]
+my_list.sort()
+print(my_list)
