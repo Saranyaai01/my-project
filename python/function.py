669d26eccddafb9792f6717a1db05db5699e92e3
Saranya N <saranyaai01@brightskills.com>
2024-02-09 11:38:49 +0530

    work out function today

---
+++ python/function.py
@@ -0,0 +1,9 @@
+def nameage():
+    name ="saranya"
+    age = 29
+    print("hai.....!",name)
+    print("hai......!",name,"my age is",age)
+    print("hai......!" +name +str(age))
+    print("hai.......!" +name+" " +str(age))
+    print("hai........!"+name+" my age is"+str(age))
+nameage()
