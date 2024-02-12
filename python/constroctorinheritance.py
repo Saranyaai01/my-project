154b6d699a8472b9379c9f86fa6cfbaa75cb1883
Saranya N <saranyaai01@brightskills.com>
2024-02-12 13:38:36 +0530

    workin in inheritance

---
+++ python/constroctorinheritance.py
@@ -0,0 +1,28 @@
+# class family():
+#     def __init__(self) :
+#         print("welcome to my family")
+#     def family_members(self):
+#         print("we are three members are there")
+# class parent():
+#     def __init__(self,mother,father):
+#         family(). __init__()
+#         self.m=mother
+#         self.f=father
+#     def welcome(self):
+#         print("my parents are Mr.",self.f,"and Mrs.",self.m)
+# class child(parent,family):
+#     def __init__(self,mother,father,son):
+#         parent. __init__(self,mother,father)
+#         self.s=son
+#     def display(self):
+#         print("I am",self.s,"son of",self.f,"and",self.m)
+# x=child("saranya","narayanan","ruthraprasath")
+# x.family_members()
+# x.welcome()
+# x.display()
+
+
+
+
+
+
