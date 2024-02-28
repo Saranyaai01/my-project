65c2d31ecde8783aade8c31a15f5b99d1ad7e721
Saranya N <saranyaai01@brightskills.com>
2024-02-28 12:27:16 +0530

    added

---
+++ python/lms/mydbfile.py
@@ -0,0 +1,58 @@
+import mysql.connector
+from tkinter import *
+class DBManipulate:
+
+
+    def returnMsg(self):
+        return "connected"
+        
+
+    def mydbconnection(self):
+        con=mysql.connector.connect(
+        host="192.168.1.240",
+        user="AIBATCH01",
+        password="AI@123",
+        database="ai_saranya"
+        )
+        return con
+
+
+    def insertvalues(self,stu_id,stu_name,book_name,author,edition,price,quatity):
+        stu_id1=stu_id
+        stu_name1=stu_name
+        book_name1=book_name
+        author1=author
+        edition1=edition
+        price1=price
+        quantity1=quatity
+
+
+       
+
+        data=self.mydbconnection()
+        result=data.cursor()
+
+        stmts="INSERT INTO borrow_details(stu_id, stu_name,book_name,author,edition,price,quantity) VALUES("+str(stu_id1)+ ", "+ '"' + stu_name1 + '"' + ","+ '"' + book_name1 + '"' + ","+ '"' + author1 + '"'  + ","+  '"' + edition1 + '"' + "," + str(price1)+" +","+ "+str(quantity1)+");)"
+
+        result.execute(stmts,multi=True)
+        print(stmts)
+
+
+
+        data.commit()
+
+        return (str(result.rowcount) + " row inserted")
+
+
+    def selectdatas(self):
+
+        data=self.mydbconnection()
+        result=data.cursor()
+        result.execute("SELECT * FROM borrow_details")
+        i=0 
+        for ai_saranya in result: 
+             for j in range(len(ai_saranya)):
+                 lbldisplay = Label(self, width=10, fg='blue') 
+                 lbldisplay.grid(row=i, column=j) 
+                 lbldisplay.insert(END, ai_saranya[j])
+             i=i+1
--- python/lms/notepad.py
+++ python/lms/notepad.py
@@ -1,34 +1,67 @@
-import tkinter as tk
-from tkinter import ttk
 import os
-demo=tk.Tk()
+from tkinter import *
+from tkinter import ttk
+from mydbfile import DBManipulate
+demo=Tk()
+demo.title("Library management system")
 demo.geometry("500x500")
-demo.title("Untitled-Notepad")
-
+dbcon=DBManipulate()
 def quit():
     demo.destroy()
 
 def about():
-    abt=tk.Tk()
+    abt=Tk()
     abt.title("About as")
     abt.geometry("300x300")
-    message="""Welcome to Notepad text editor
+    message="""Welcome to libraray management system
     created on :22-02-2024
     by saranya N
     """
-    lntinfo=tk.Label(abt,text=message)
+    lntinfo=Label(abt,text=message)
     lntinfo.pack()
     abt.mainloop()
 
 
-menubar=tk.Menu(demo)
-submenulist=tk.Menu(menubar,tearoff=0)
+
+    #connection
+
+def inserttoDB():
+    stu_id1=str(Ety_Stdid.get())
+    stu_name1=str(Ety_StdName.get())
+    book_name1=str(Ety_bookname.get())
+    author1=str(Ety_author_name.get())
+    edition1=str(Ety_Edition.get())
+    price1=str(Ety_price.get())
+    quantity1=str(Ety_quantity.get())
+
+    x = dbcon.insertvalues(stu_id1,stu_name1,book_name1,author1,edition1,price1,quantity1)
+    lblConMsg.config(text=x)
+    # selectdatas()
+
+
+# def selectdatas():
+
+#     data=mydbcon.mydbconnection()
+#     result=data.cursor()
+#     result.execute("SELECT * FROM borrow_details")
+#     i=0 
+#     for ai_saranya in result: 
+#         for j in range(len(ai_saranya)):
+#             lbldisplay = Entry(resultframe, width=10, fg='blue') 
+#             lbldisplay.grid(row=i, column=j) 
+#             lbldisplay.insert(END, ai_saranya[j])
+#         i=i+1 
+
+
+
+menubar=Menu(demo)
+submenulist=Menu(menubar,tearoff=0)
 
 submenulist.add_command(label="New File", underline=0,accelerator="Ctr+N")
 submenulist.add_command(label="New project", underline=7, accelerator="Ctrl+Shift+J")
 submenulist.add_command(label="New Py File", underline=5, accelerator="Ctrl+N")
 
-filemenu=tk.Menu(menubar,tearoff=0)
+filemenu=Menu(menubar,tearoff=0)
 menubar.add_cascade(label="File",menu=filemenu, underline=0)
 
 filemenu.add_cascade(label="New",underline=0, accelerator="Ctr+N")
@@ -42,7 +75,7 @@ filemenu.add_separator()
 filemenu.add_command(label="Exit",underline=0,command=quit)
 
 
-editmenu=tk.Menu(menubar, tearoff=0)
+editmenu=Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Edit", menu=editmenu,underline=0)
 
 editmenu.add_command(label="Undo", underline=0,accelerator="Crl+Z")
@@ -60,18 +93,18 @@ editmenu.add_separator()
 editmenu.add_command(label="Select All", underline=0,accelerator="Crl+A")
 editmenu.add_command(label="Time/Date", underline=0,accelerator="F5")
 
-formatmenu=tk.Menu(menubar, tearoff=0)
+formatmenu=Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Format",menu=formatmenu,underline=0)
 
 formatmenu.add_command(label="Word Wrap", underline=0)
 formatmenu.add_command(label="Font...", underline=0)
 
-viewmenu=tk.Menu(menubar, tearoff=0)
+viewmenu=Menu(menubar, tearoff=0)
 menubar.add_cascade(label="View",menu=viewmenu,underline=0)
 
 viewmenu.add_cascade(label="Status Bar", underline=0)
 
-helpmenu=tk.Menu(menubar,tearoff=0)
+helpmenu=Menu(menubar,tearoff=0)
 menubar.add_cascade(label="Help",menu=helpmenu,underline=0)
 
 helpmenu.add_command(label="View Help",underline=0)
@@ -88,15 +121,20 @@ helpmenu.add_command(label="About Notepad", underline=0,command=about)
 
 demo.config(menu=menubar)
 
-# imgdir=os.path.join(os.path.dirname(__file__),'ims')
-# getTitleImage=tk.PhotoImage('titleimage',file=os.path.join(imgdir,'ims.gif'))
 
-titleImageFrame=tk.Frame(demo,bg="blue")
+imgdir1=os.path.join((os.path.join(os.path.dirname(__file__),'img')),"lms.gif")
+getTitleImage=PhotoImage('titleimage',file=imgdir1)
+
+
+# imgdir=os.path.join(os.path.dirname(__file__),'ims')
+# getTitleImage=tk.PhotoImage('titleimage',file=os.path.join(imgdir,'lms.gif'))
+
+titleImageFrame=Frame(demo,bg="blue")
 titleImageFrame.pack(padx=10,fill="x")
 
-# lblDisplayTitleImage=tk.Label(titleImageFrame,image=getTitleImage).pack()
+lblDisplayTitleImage=Label(titleImageFrame,image=getTitleImage).pack()
 
-lblDisplayTitleImage=tk.Label(text="library management system").pack()
+# lblDisplayTitleImage=Label(text="library management system").pack()
 
 tablist=ttk.Notebook(demo)
 tablist.pack(padx=10,pady=5)
@@ -117,54 +155,68 @@ tablist.add(tabDelete,text="Delete")
 titledisplayframeintab=ttk.Frame(tabInsert,width=demo.winfo_screenwidth(), height=demo.winfo_screenheight())
 titledisplayframeintab.pack()
 
-lbl_insertTitle=tk.Label(titledisplayframeintab, text="Inserting Student Details")
+lbl_insertTitle=Label(titledisplayframeintab, text="Inserting Student book_borrow Details")
 lbl_insertTitle.grid(pady=10,row=0, columnspan=10)
 
 
 
-lbl_StdName=tk.Label(titledisplayframeintab,text="Name of the student ")
-lbl_StdName.grid(pady=10,row=1,column=1)
-Ety_StdName=tk.Entry(titledisplayframeintab)
-Ety_StdName.grid(padx=10,pady=10,row=1, column=2)
+lbl_Stdid=Label(titledisplayframeintab,text="Student ID")
+lbl_Stdid.grid(pady=10,row=1,column=1)
+Ety_Stdid=Entry(titledisplayframeintab)
+Ety_Stdid.grid(padx=10,pady=10,row=1, column=2)
 
 
-lbl_Stdid=tk.Label(titledisplayframeintab,text="Roll Number ")
-lbl_Stdid.grid(pady=10,row=2,column=1)
-Ety_Stdid=tk.Entry(titledisplayframeintab)
-Ety_Stdid.grid(padx=10,pady=10,row=2, column=2)
+lbl_Stdname=Label(titledisplayframeintab,text="Name of the student ")
+lbl_Stdname.grid(pady=10,row=2,column=1)
+Ety_StdName=Entry(titledisplayframeintab)
+Ety_StdName.grid(padx=10,pady=10,row=2, column=2)
 
-lbl_bookname=tk.Label(titledisplayframeintab,text="Name of the book ")
+lbl_bookname=Label(titledisplayframeintab,text="Name of the book ")
 lbl_bookname.grid(pady=10,row=3,column=1)
-Ety_bookname=tk.Entry(titledisplayframeintab)
+Ety_bookname=Entry(titledisplayframeintab)
 Ety_bookname.grid(padx=10,pady=10,row=3, column=2)
 
-lbl_author_name=tk.Label(titledisplayframeintab,text="Name of the Author ")
+lbl_author_name=Label(titledisplayframeintab,text="Name of the Author ")
 lbl_author_name.grid(pady=10,row=4,column=1)
-Ety_author_name=tk.Entry(titledisplayframeintab)
+Ety_author_name=Entry(titledisplayframeintab)
 Ety_author_name.grid(padx=10,pady=10,row=4, column=2)
 
-lbl_Edition=tk.Label(titledisplayframeintab,text="Edition ")
+lbl_Edition=Label(titledisplayframeintab,text="Edition ")
 lbl_Edition.grid(pady=10,row=5,column=1)
-Ety_Edition=tk.Entry(titledisplayframeintab)
+Ety_Edition=Entry(titledisplayframeintab)
 Ety_Edition.grid(padx=10,pady=10,row=5, column=2)
 
-
-
-lbl_quantity=tk.Label(titledisplayframeintab,text="Quantity ")
-lbl_quantity.grid(pady=10,row=6,column=1)
-Ety_quantity=tk.Entry(titledisplayframeintab)
-Ety_quantity.grid(padx=10,pady=10,row=6, column=2)
-
-btn_Insert=tk.Button(titledisplayframeintab, text="Insert")
-btn_Insert.grid(row=7,column=1)
-
-btn_Clear=tk.Button(titledisplayframeintab, text="Clear")
-btn_Clear.grid(row=7,column=2)
-
-btn_Exit=tk.Button(titledisplayframeintab, text="Quit", command=quit)
-btn_Exit.grid(row=7,column=3)
+lbl_price=Label(titledisplayframeintab,text="price ")
+lbl_price.grid(pady=10,row=6,column=1)
+Ety_price=Entry(titledisplayframeintab)
+Ety_price.grid(padx=10,pady=10,row=6, column=2)
 
 
 
+lbl_quantity=Label(titledisplayframeintab,text="Quantity ")
+lbl_quantity.grid(pady=10,row=7,column=1)
+Ety_quantity=Entry(titledisplayframeintab)
+Ety_quantity.grid(padx=10,pady=10,row=7, column=2)
+
+btn_Insert=Button(titledisplayframeintab, text="Insert",command=inserttoDB)
+btn_Insert.grid(row=8,column=1)
+
+btn_Clear=Button(titledisplayframeintab, text="Clear")
+btn_Clear.grid(row=8,column=2)
+
+btn_Exit=Button(titledisplayframeintab, text="Quit", command=quit)
+btn_Exit.grid(row=8,column=3)
+
+msg=dbcon.mydbconnection()
+lblConMsg=Label(titledisplayframeintab, text=msg)
+lblConMsg.grid(row=9,column=2, pady=20)
+
+# lblConMsg=Label(titledisplayframeintab, text=" ")
+
+
+resultframe=Frame(titledisplayframeintab,width=800, height=700,bg="yellow")
+resultframe.grid(row=10, columnspan=6)
+
+#updatading details
 
 demo.mainloop()
